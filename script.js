5445a5301d3dadcb7db750da6b407a2e2258731d
Victor <vitoralmeida95@hotmail.com>
2022-10-04 17:36:47 -0300

    ate a 8 com excessão do 5

--- script.js
+++ script.js
@@ -12,16 +12,11 @@ function generateColor() {
     
 }
 
-// seta cores
+// botão 
 let rand1 = document.getElementById('rand1')
 let rand2 = document.getElementById('rand2')
 let rand3 = document.getElementById('rand3')
 
-rand1.style.background = generateColor()
-rand2.style.background = generateColor()
-rand3.style.background = generateColor()
-
-// botão 
 let botaoR = document.getElementById('button-random-color')
 botaoR.addEventListener('click', randomizaCor)
 function randomizaCor(){
@@ -29,3 +24,31 @@ function randomizaCor(){
     rand2.style.background = generateColor()
     rand3.style.background = generateColor()
 }   
+
+// seta cores
+// let cor1 = document.querySelector('rand1.style.backgroundColor')
+// let cor2 = rand2.style.backgroundColor
+// let cor3 = rand3.style.backgroundColor
+
+
+// localStorage.setItem('color1',cor1)
+// localStorage.setItem('color2',cor2)
+// localStorage.setItem('color3',cor3)
+
+// localStorage.getItem('color1')
+// localStorage.getItem('color2')
+// localStorage.getItem('color3')
+
+// gera quadrado
+for (let index = 1; index <= 25; index += 1){
+    let borda = document.getElementById('pixel-board')
+    let pixelCreate = document.createElement('p')
+    pixelCreate.className = 'pixel'
+    pixelCreate.style.backgroundColor = 'white'
+    borda.appendChild(pixelCreate)
+}
+
+// seleciona preto
+
+let preto = document.getElementById('preto')
+preto.classList.add('selected')
