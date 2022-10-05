d41ff2f654610bdafadc24628c0b112f9e778262
Victor <vitoralmeida95@hotmail.com>
2022-10-05 00:02:17 -0300

    ate o 9

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ function generateColor() {
     return color;
     
 }
-
 // botão 
+
 let rand1 = document.getElementById('rand1')
 let rand2 = document.getElementById('rand2')
 let rand3 = document.getElementById('rand3')
@@ -20,24 +20,16 @@ let rand3 = document.getElementById('rand3')
 let botaoR = document.getElementById('button-random-color')
 botaoR.addEventListener('click', randomizaCor)
 function randomizaCor(){
-    rand1.style.background = generateColor()
-    rand2.style.background = generateColor()
-    rand3.style.background = generateColor()
-}   
-
-// seta cores
-// let cor1 = document.querySelector('rand1.style.backgroundColor')
-// let cor2 = rand2.style.backgroundColor
-// let cor3 = rand3.style.backgroundColor
-
-
-// localStorage.setItem('color1',cor1)
-// localStorage.setItem('color2',cor2)
-// localStorage.setItem('color3',cor3)
-
-// localStorage.getItem('color1')
-// localStorage.getItem('color2')
-// localStorage.getItem('color3')
+    cor1 = generateColor()
+    cor2 = generateColor()
+    cor3 = generateColor()
+    localStorage.setItem('color1',cor1)
+    localStorage.setItem('color2',cor2)
+    localStorage.setItem('color3',cor3)
+    rand1.style.background = cor1
+    rand2.style.background = cor2
+    rand3.style.background = cor3
+}
 
 // gera quadrado
 for (let index = 1; index <= 25; index += 1){
@@ -52,3 +44,36 @@ for (let index = 1; index <= 25; index += 1){
 
 let preto = document.getElementById('preto')
 preto.classList.add('selected')
+
+// add e remove propriedade de select)
+preto.addEventListener('click', ColorPixel)
+rand1.addEventListener('click', ColorPixel1);
+rand2.addEventListener('click', ColorPixel2);
+rand3.addEventListener('click', ColorPixel3);
+
+function ColorPixel(){
+    preto.classList.add('selected')
+    rand1.classList.remove('selected')
+    rand2.classList.remove('selected')
+    rand3.classList.remove('selected')
+}
+function ColorPixel1(){
+    preto.classList.remove('selected')
+    rand1.classList.add('selected')
+    rand2.classList.remove('selected')
+    rand3.classList.remove('selected')
+}
+function ColorPixel2(){
+    preto.classList.remove('selected')
+    rand1.classList.remove('selected')
+    rand2.classList.add('selected')
+    rand3.classList.remove('selected')
+}
+function ColorPixel3(){
+    preto.classList.remove('selected')
+    rand1.classList.remove('selected')
+    rand2.classList.remove('selected')
+    rand3.classList.add('selected')
+}
+// pinta quadrados
+
